30106c13caccd32b99e566aac22edc5e7d118614
Ali Raheem <ali@hadoken.cc>
2018-11-06 23:13:31 +0000

    Added better proxy support

--- bestmixer.py
+++ bestmixer.py
@@ -6,14 +6,18 @@
 
 import json
 import requests
+import base64
 
 class BestMixer:
-    def __init__(self, api_key, tor = None, tor_port = 9150):
+    def __init__(self, api_key, tor = None, proxy = 'socks5://localhost:9150', use_proxy = False):
         self.api_key = api_key
         self.url = 'http://bestmixer7o57mba.onion' if tor else 'https://bestmixer.io'
         self.url += '/api/ext'
-        #9150 is port for TBB, tor uses 9050
-        self.proxies = {'http': 'socks5://localhost:'+str(tor_port)} if tor else {}
+        self.proxies = None
+        if tor or use_proxy:
+            self.proxies = {'http': proxy}
+        else:
+            self.proxies = {}
     def request(self, action, data):
         data['api_key'] = self.api_key
         data = json.dumps(data)
@@ -34,23 +38,28 @@ class BestMixer:
         data['coin'] = coin
 # currently accepted ltc, btc, bch soon eth no validation incase
         data['fee'] = fee
+# total percent must add up to 100
+        total_percent = 0
+        for out in output:
+            total_percent += out['percent']
+        if(total_percent != 100):
+            raise Exception('Outputs in Order must total 100%')
+
         data['output'] = output
         if(bm_code):
             data['bm_code'] = bm_code
         return self.request('/order/create', data)
+    @staticmethod
+    def output(address, percent = 100, delay = 0):
+        return {'address': address, 'percent': percent, 'delay': delay}
 
 if __name__ == '__main__':
-    bm = BestMixer("replace_with_api_key")
-    # Example two address LTC mix with 30/70 split 100mins and 400 min delays, 0.5612% fee
-    print(bm.orderCreate('ltc', 0.5612,
+    bm = BestMixer('replace_with_API_key')
+    # Example two address LTC mix with 30/70 split half hour and 2hr delays, 0.5612% fee
+    order = bm.orderCreate('ltc', 0.5612,
     [
-        {'address': 'Lxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-         'percent': 30,
-         'delay': 55
-        },
-        {'address': 'Lxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-         'percent': 70,
-         'delay': 120
-        }
+        BestMixer.output('Lxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 30, 30),
+        BestMixer.output('Lxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 70, 120)
     ]
-    ))
+    )
+    print(base64.b64decode(order['data']['letter_of_guarantee']).decode())
